aaa7e985fa15fa10916717dff178e62fe9e62987
Satrey <satrey.mail@gmail.com>
2023-01-10 16:37:19 +0500

    end main part of project

--- django4/blog/views.py
+++ django4/blog/views.py
@@ -1,18 +1,12 @@
-from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
 def post_list(request):
     post = Post.published.all()
-    return render(request, 'blog/list.html', {'post': post})
+    return render(request, 'blog/post/list.html', {'post': post})
 
 
 def post_detail(request, id):
-    try:
-        post = Post.published.get(id=id)
-
-    except Post.DoesNotExist:
-        raise Http404('Not Found')
-
-    return render(request, 'blog/detail.html', {'post': post})
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    return render(request, 'blog/post/detail.html', {'post': post})
--- django4/mysite/urls.py
+++ django4/mysite/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from setuptools import namespaces
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    path('blog/', include('blog.urls', namespace='blog'))
 ]
