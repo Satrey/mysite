212af53bcff52ab538a7bb61517598321c199ad9
Satrey <satrey.mail@gmail.com>
2023-01-11 17:01:25 +0500

    Реализовано представление PostListView на основе класса ListView. Представление на основе функции post_list закоментировано.

--- django4/blog/views.py
+++ django4/blog/views.py
@@ -1,20 +1,21 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.views.generic import ListView
 
 
-def post_list(request):
-    post_list = Post.published.all()
-    paginator = Paginator(post_list, 4)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-
-    return render(request, 'blog/post/list.html', {'posts': posts})
+# def post_list(request):
+#     post_list = Post.published.all()
+#     paginator = Paginator(post_list, 4)
+#     page_number = request.GET.get('page', 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#
+#     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
@@ -25,3 +26,12 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+class PostListView(ListView):
+    """Альтернативный вид основанный на классах"""
+
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 4
+    template_name = 'blog/post/list.html'
