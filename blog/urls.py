5a5200ca131bae25515245ab715bf845ff482cea
Alex Pokrashenko <satrey.mail@gmail.com>
2025-11-01 13:37:25 +0500

    str72 Class based view

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,10 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+         views.post_detail,
+         name='post_detail'
+    ),
     path('draft/', views.post_list_draft, name='post_list_draft'),
-    path('draft/<int:id>', views.post_detail_draft, name='post_detail_draft'),
-    path('published/', views.post_list_published, name='post_list_published'),
-    path('published/<int:id>/', views.post_detail_published, name='post_detail_published')
+    path('published/', views.PostListView.as_view(), name='post_list_published'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 from .models import Post
 
 
 def post_list_published(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+        
     return render(
         request,
         'blog/post/post_list.html',
-        {'post', posts}
+        {'posts': posts}
     )
 
 def post_list_draft(request):
@@ -17,14 +28,17 @@ def post_list_draft(request):
     return render(
         request,
         'blog/post/post_list.html',
-        {'post', posts}
+        {'posts': posts}
     )
 
-def post_detail_published(request, id):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post,
-        id=id,
-        status=Post.Status.PUBLISHED
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
     )
     return render(
         request,
@@ -32,14 +46,15 @@ def post_detail_published(request, id):
         {'post': post}
     )
 
-def post_detail_draft(request, id):
-    post = get_object_or_404(
-        Post,
-        id=id,
-        status=Post.Status.DRAFT
-    )
-    return render(
-        request,
-        'blog/poist/post_detail.html',
-        {'post': post}
-    )
+
+class PostListView(ListView):
+    """
+    Альтернативное представление для post_list
+    Args:
+        ListView (_type_): _description_
+    """
+    
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/post_list.html'
